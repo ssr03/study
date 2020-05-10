6cd9901905631a6894c17795d56df44a95312791
ssr03 <ssr03@gmail.com>
2020-05-11 08:20:12 +0900

    real world vue 예제

---
+++ vue/exercise/real-world-vue/src/components/EventCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 @click="handler(arg, event)">제목</h1>
+    <div>
+      <ul>
+        <li>list</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { libName } from 'libName'
+export default {
+  data() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
+}
+</script>
+
+<style></style>
